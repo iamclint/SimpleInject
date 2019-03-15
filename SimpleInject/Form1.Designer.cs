5e58909dbd829ca698e5fba3536f36fbea0e8da7
iamclint <clint.ban@gmail.com>
2019-03-15 16:48:57 -0500

    Add project files.

---
+++ SimpleInject/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace SimpleInject
+{
+    partial class SimpleInject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleInject));
+            this.DllX86 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x86Browse = new System.Windows.Forms.Button();
+            this.x64Browse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DllX64 = new System.Windows.Forms.TextBox();
+            this.ProcessList = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colProcessor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.injectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forceClosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enumerateModulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Filters = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ProcessListTmr = new System.Windows.Forms.Timer(this.components);
+            this.hideEmptyTitles = new System.Windows.Forms.CheckBox();
+            this.NotifyMessage = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lvContext.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DllX86
+            // 
+            this.DllX86.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DllX86.Location = new System.Drawing.Point(42, 12);
+            this.DllX86.Name = "DllX86";
+            this.DllX86.Size = new System.Drawing.Size(744, 20);
+            this.DllX86.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x86";
+            // 
+            // x86Browse
+            // 
+            this.x86Browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.x86Browse.Location = new System.Drawing.Point(792, 10);
+            this.x86Browse.Name = "x86Browse";
+            this.x86Browse.Size = new System.Drawing.Size(29, 23);
+            this.x86Browse.TabIndex = 2;
+            this.x86Browse.Text = "...";
+            this.x86Browse.UseVisualStyleBackColor = true;
+            this.x86Browse.Click += new System.EventHandler(this.X86Browse_Click);
+            // 
+            // x64Browse
+            // 
+            this.x64Browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.x64Browse.Location = new System.Drawing.Point(792, 36);
+            this.x64Browse.Name = "x64Browse";
+            this.x64Browse.Size = new System.Drawing.Size(29, 23);
+            this.x64Browse.TabIndex = 5;
+            this.x64Browse.Text = "...";
+            this.x64Browse.UseVisualStyleBackColor = true;
+            this.x64Browse.Click += new System.EventHandler(this.X64Browse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "x64";
+            // 
+            // DllX64
+            // 
+            this.DllX64.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DllX64.Location = new System.Drawing.Point(42, 38);
+            this.DllX64.Name = "DllX64";
+            this.DllX64.Size = new System.Drawing.Size(744, 20);
+            this.DllX64.TabIndex = 3;
+            // 
+            // ProcessList
+            // 
+            this.ProcessList.AllowColumnReorder = true;
+            this.ProcessList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProcessList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colTitle,
+            this.colId,
+            this.colProcessor});
+            this.ProcessList.ContextMenuStrip = this.lvContext;
+            this.ProcessList.FullRowSelect = true;
+            this.ProcessList.Location = new System.Drawing.Point(12, 64);
+            this.ProcessList.MultiSelect = false;
+            this.ProcessList.Name = "ProcessList";
+            this.ProcessList.Size = new System.Drawing.Size(809, 324);
+            this.ProcessList.TabIndex = 6;
+            this.ProcessList.UseCompatibleStateImageBehavior = false;
+            this.ProcessList.View = System.Windows.Forms.View.Details;
+            this.ProcessList.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ProcessList_ColumnClick);
+            this.ProcessList.DoubleClick += new System.EventHandler(this.ProcessList_DoubleClick);
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 48;
+            // 
+            // colTitle
+            // 
+            this.colTitle.Text = "Title";
+            this.colTitle.Width = 603;
+            // 
+            // colId
+            // 
+            this.colId.Text = "ID";
+            // 
+            // colProcessor
+            // 
+            this.colProcessor.Text = "x86/x64";
+            // 
+            // lvContext
+            // 
+            this.lvContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.injectToolStripMenuItem,
+            this.enumerateModulesToolStripMenuItem,
+            this.forceClosToolStripMenuItem});
+            this.lvContext.Name = "lvContext";
+            this.lvContext.Size = new System.Drawing.Size(181, 92);
+            // 
+            // injectToolStripMenuItem
+            // 
+            this.injectToolStripMenuItem.Name = "injectToolStripMenuItem";
+            this.injectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.injectToolStripMenuItem.Text = "&Inject";
+            this.injectToolStripMenuItem.Click += new System.EventHandler(this.InjectToolStripMenuItem_Click);
+            // 
+            // forceClosToolStripMenuItem
+            // 
+            this.forceClosToolStripMenuItem.Name = "forceClosToolStripMenuItem";
+            this.forceClosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.forceClosToolStripMenuItem.Text = "&Force close";
+            this.forceClosToolStripMenuItem.Click += new System.EventHandler(this.ForceClosToolStripMenuItem_Click);
+            // 
+            // enumerateModulesToolStripMenuItem
+            // 
+            this.enumerateModulesToolStripMenuItem.Name = "enumerateModulesToolStripMenuItem";
+            this.enumerateModulesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.enumerateModulesToolStripMenuItem.Text = "&Enumerate Modules";
+            // 
+            // Filters
+            // 
+            this.Filters.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Filters.Location = new System.Drawing.Point(52, 412);
+            this.Filters.Name = "Filters";
+            this.Filters.Size = new System.Drawing.Size(769, 20);
+            this.Filters.TabIndex = 7;
+            this.Filters.TextChanged += new System.EventHandler(this.Filters_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 415);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Filters";
+            // 
+            // ProcessListTmr
+            // 
+            this.ProcessListTmr.Enabled = true;
+            this.ProcessListTmr.Interval = 500;
+            this.ProcessListTmr.Tick += new System.EventHandler(this.ProcessListTmr_Tick);
+            // 
+            // hideEmptyTitles
+            // 
+            this.hideEmptyTitles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.hideEmptyTitles.AutoSize = true;
+            this.hideEmptyTitles.Location = new System.Drawing.Point(14, 389);
+            this.hideEmptyTitles.Name = "hideEmptyTitles";
+            this.hideEmptyTitles.Size = new System.Drawing.Size(103, 17);
+            this.hideEmptyTitles.TabIndex = 9;
+            this.hideEmptyTitles.Text = "Hide empty titles";
+            this.hideEmptyTitles.UseVisualStyleBackColor = true;
+            this.hideEmptyTitles.CheckedChanged += new System.EventHandler(this.HideEmptyTitles_CheckedChanged);
+            // 
+            // NotifyMessage
+            // 
+            this.NotifyMessage.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyMessage.Icon")));
+            this.NotifyMessage.Text = "Simple Inject";
+            this.NotifyMessage.Visible = true;
+            // 
+            // SimpleInject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 443);
+            this.Controls.Add(this.hideEmptyTitles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Filters);
+            this.Controls.Add(this.ProcessList);
+            this.Controls.Add(this.x64Browse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DllX64);
+            this.Controls.Add(this.x86Browse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DllX86);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SimpleInject";
+            this.Text = "Simple Inject";
+            this.lvContext.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DllX86;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button x86Browse;
+        private System.Windows.Forms.Button x64Browse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DllX64;
+        private System.Windows.Forms.ListView ProcessList;
+        private System.Windows.Forms.TextBox Filters;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colTitle;
+        private System.Windows.Forms.ColumnHeader colId;
+        private System.Windows.Forms.ColumnHeader colProcessor;
+        private System.Windows.Forms.Timer ProcessListTmr;
+        private System.Windows.Forms.CheckBox hideEmptyTitles;
+        private System.Windows.Forms.NotifyIcon NotifyMessage;
+        private System.Windows.Forms.ContextMenuStrip lvContext;
+        private System.Windows.Forms.ToolStripMenuItem injectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forceClosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enumerateModulesToolStripMenuItem;
+    }
+}
+
---
+++ SimpleInject/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace SimpleInject
+{
+    public partial class SimpleInject : Form
+    {
+
+        [DllImport("libinj.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Is32Bit(int process_id);
+        [DllImport("libinj.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Inject(int process_id, string dll, int method, IntPtr handle = default(IntPtr));
+        [DllImport("libinj.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern string getLastError();
+
+        public static BackgroundWorker UpdateThread;
+        private ColumnHeader SortingColumn = null;
+
+        private bool[] sortColAsc;
+
+
+        public SimpleInject()
+        {
+            InitializeComponent();
+            loadSettings();
+            UpdateThread_DoWork(null, null); // initial open don't use background worker its slower at updating the list due to invoking.
+            UpdateThread = new BackgroundWorker();
+            UpdateThread.DoWork += new System.ComponentModel.DoWorkEventHandler(this.UpdateThread_DoWork);
+        }
+
+        private void loadSettings()
+        {
+            DllX86.Text =  Properties.Settings.Default.X86;
+            DllX64.Text = Properties.Settings.Default.X64;
+            hideEmptyTitles.Checked = Properties.Settings.Default.HideEmptyTitles;
+            Filters.Text = Properties.Settings.Default.Filters;
+
+            if (sortColAsc == null)
+            {
+                sortColAsc = new bool[20];
+                foreach (ColumnHeader ch in ProcessList.Columns)
+                    sortColAsc[ch.Index] = false;
+            }
+        }
+
+        private bool isValidFile(string file_path)
+        {
+            if (!File.Exists(file_path))
+                return false;
+            return true;
+        }
+
+        private void UpdateThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            bool hasChanged = false;
+
+            foreach (Process p in Process.GetProcesses())
+            {
+                bool isExisting=false;
+                bool isMatching = filterProcess(p);
+
+                if (ProcessList.InvokeRequired)
+                    ProcessList.Invoke(new MethodInvoker(delegate { isExisting = ProcessList.Items.ContainsKey(p.Id.ToString()); }));
+                else
+                    isExisting = ProcessList.Items.ContainsKey(p.Id.ToString());
+
+                if (hideEmptyTitles.Checked && p.MainWindowTitle.Trim() == "")
+                    isMatching = false;
+
+                if (!isExisting && isMatching)
+                {
+                    ListViewItem lvi = new ListViewItem(new[] { p.ProcessName, p.MainWindowTitle, p.Id.ToString(), (Is32Bit(p.Id)?"X86":"X64") });
+                    lvi.Name = p.Id.ToString(); //set the key of the item here
+                    if (ProcessList.InvokeRequired)
+                        ProcessList.Invoke(new MethodInvoker(delegate
+                        {
+                            ProcessList.Items.Add(lvi);
+                        }));
+                    else
+                        ProcessList.Items.Add(lvi);
+                    hasChanged = true;
+                }
+                else
+                {
+                    string title = "";
+                    if (isExisting)
+                    {
+                        if (ProcessList.InvokeRequired)
+                            ProcessList.Invoke(new MethodInvoker(delegate
+                            {
+                                title = ProcessList.Items[p.Id.ToString()].SubItems[1].Text;
+                            }));
+                        else
+                            title = ProcessList.Items[p.Id.ToString()].SubItems[1].Text;
+                    }
+
+                    if (isExisting && title != p.MainWindowTitle) //if you always update without checking it causes all the items to flicker
+                    {
+                        if (ProcessList.InvokeRequired)
+                            ProcessList.Invoke(new MethodInvoker(delegate
+                            {
+                                ProcessList.Items[p.Id.ToString()].SubItems[1].Text = p.MainWindowTitle;
+                            }));
+                        else
+                            ProcessList.Items[p.Id.ToString()].SubItems[1].Text = p.MainWindowTitle;
+                        hasChanged = true;
+                    }
+
+                    if (!isMatching && isExisting)
+                    {
+                        if (ProcessList.InvokeRequired)
+                            ProcessList.Invoke(new MethodInvoker(delegate
+                            {
+                                ProcessList.Items.RemoveAt(ProcessList.Items[p.Id.ToString()].Index);
+                            }));
+                        else 
+                            ProcessList.Items.RemoveAt(ProcessList.Items[p.Id.ToString()].Index);
+                    }
+                }
+            }
+            if (hasChanged)
+                if (ProcessList.InvokeRequired)
+                    ProcessList.Invoke(new MethodInvoker(delegate { ProcessList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent); }));
+                else
+                    ProcessList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        public bool filterProcess(Process p)
+        {
+            
+            string[] filterArr = Filters.Text.Split(';');
+            foreach (string filterStr in filterArr)
+            {
+                if (p.ProcessName.ToLower().IndexOf(filterStr.ToLower()) >= 0)
+                    return true;
+                if (p.MainWindowTitle.ToLower().IndexOf(filterStr) >= 0)
+                    return true;
+            }   
+            return false;
+        }
+
+        private void ProcessListTmr_Tick(object sender, EventArgs e)
+        {
+            if (!UpdateThread.IsBusy)
+                UpdateThread.RunWorkerAsync();
+        }
+
+        private void ProcessList_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            ColumnHeader new_sorting_column = ProcessList.Columns[e.Column];
+            System.Windows.Forms.SortOrder sort_order;
+            if (SortingColumn == null)
+            {
+                // New column. Sort ascending.
+                sort_order = SortOrder.Ascending;
+            }
+            else
+            {
+                // See if this is the same column.
+                if (new_sorting_column == SortingColumn)
+                {
+                    // Same column. Switch the sort order.
+                    if (sortColAsc[e.Column])
+                    {
+                        sort_order = SortOrder.Descending;
+                    }
+                    else
+                    {
+                        sort_order = SortOrder.Ascending;
+                    }
+                }
+                else
+                {
+                    // New column. Sort ascending.
+                    sort_order = SortOrder.Ascending;
+                }
+
+                // Remove the old sort indicator.
+                // SortingColumn.Text = SortingColumn.Text.Substring(2);
+            }
+
+    
+
+
+            SortingColumn = new_sorting_column;
+
+
+            // Create a comparer.
+            ProcessList.ListViewItemSorter = new ListViewComparer(e.Column, sort_order);
+
+            if (sort_order == SortOrder.Ascending)
+                sortColAsc[e.Column] = true;
+            else
+                sortColAsc[e.Column] = false;
+
+            // Sort.
+            ProcessList.Sort();
+        }
+        private void ProcessList_DoubleClick(object sender, EventArgs e)
+        {
+            int process_id = int.Parse(ProcessList.SelectedItems[0].SubItems[GetColumnIndex("id")].Text);
+            Inject(process_id);
+           
+        }
+
+        private void Inject(int process_id)
+        {
+            string balloonText;
+            if (Is32Bit(process_id))
+            {
+                if (isValidFile(DllX86.Text))
+                {
+                    balloonText = DllX86.Text.Substring(DllX86.Text.LastIndexOf(@"\") + 1, DllX86.Text.Length - DllX86.Text.LastIndexOf(@"\") - 1) + "-> " + ProcessList.SelectedItems[0].SubItems[0].Text.ToString();
+                    if (!Inject(process_id, DllX86.Text, 1))
+                        NotifyMessage.ShowBalloonTip(3000, "Error", getLastError(), ToolTipIcon.Error);
+                    else
+                        NotifyMessage.ShowBalloonTip(3000, "Successful Injection", balloonText, ToolTipIcon.Info);
+
+                }
+                else
+                {
+                    NotifyMessage.ShowBalloonTip(3000, "Error", "Invalid file selected!", ToolTipIcon.Error);
+                }
+            }
+            else
+            {
+                if (isValidFile(DllX64.Text))
+                {
+                    balloonText = DllX64.Text.Substring(DllX64.Text.LastIndexOf(@"\") + 1, DllX64.Text.Length - DllX64.Text.LastIndexOf(@"\") - 1) + "-> " + ProcessList.SelectedItems[0].SubItems[0].Text.ToString();
+                    if (!Inject(process_id, DllX64.Text, 1))
+                        NotifyMessage.ShowBalloonTip(3000, "Error", getLastError(), ToolTipIcon.Error);
+                    else
+                        NotifyMessage.ShowBalloonTip(3000, "Successful Injection", balloonText, ToolTipIcon.Info);
+                }
+            }
+        }
+
+        private int GetColumnIndex(string name)
+        {
+            foreach (ColumnHeader col in ProcessList.Columns)
+            {
+                if (col.Text.ToLower() == name.ToLower())
+                    return col.Index;
+            }
+            return -1;
+        }
+
+        private void X86Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ld = new System.Windows.Forms.OpenFileDialog();
+            {
+                var withBlock = ld;
+                withBlock.DefaultExt = ".dll";
+                withBlock.Filter = "Dll Files|*.dll";
+                if (Properties.Settings.Default.X86.LastIndexOf(@"\") > 0)
+                    withBlock.InitialDirectory = Properties.Settings.Default.X86.Substring(0, Properties.Settings.Default.X86.LastIndexOf(@"\"));
+            }
+            if (ld.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DllX86.Text = ld.FileName;
+                Properties.Settings.Default.X86 = ld.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void X64Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ld = new System.Windows.Forms.OpenFileDialog();
+            {
+                var withBlock = ld;
+                withBlock.DefaultExt = ".dll";
+                withBlock.Filter = "Dll Files|*.dll";
+                if (Properties.Settings.Default.X64.LastIndexOf(@"\") > 0)
+                    withBlock.InitialDirectory = Properties.Settings.Default.X64.Substring(0, Properties.Settings.Default.X64.LastIndexOf(@"\"));
+            }
+            if (ld.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DllX86.Text = ld.FileName;
+                Properties.Settings.Default.X64 = ld.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void HideEmptyTitles_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.HideEmptyTitles = hideEmptyTitles.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Filters_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Filters = Filters.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void InjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int process_id = int.Parse(ProcessList.SelectedItems[0].SubItems[GetColumnIndex("id")].Text);
+            Inject(process_id);
+        }
+
+        private void ForceClosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int process_id = int.Parse(ProcessList.SelectedItems[0].SubItems[GetColumnIndex("id")].Text);
+            Process p = Process.GetProcessById(process_id);
+            p.Kill();
+        }
+    }
+}
